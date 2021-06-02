ca4bafa1d964bec454fedd6d1a1b113f4ad680c6
shechem24 <wlsrmssla@naver.com>
2021-06-02 15:03:01 +0900

    회원가입 페이지 구현

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -8,6 +8,7 @@ const ctrl = require("./home.ctrl");
 // API 만들기
 router.get("/", ctrl.output.home);
 router.get("/login", ctrl.output.login);
+router.get("/register", ctrl.output.register);
 router.post("/login", ctrl.process.login);
 
 module.exports = router;
