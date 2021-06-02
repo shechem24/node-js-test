2bd1c5b74c02f29c795b00e20cd001497c5611d2
shechem24 <wlsrmssla@naver.com>
2021-06-02 14:38:18 +0900

    로그인 모듈 생성

---
+++ app/src/models/UserStorage.js
@@ -0,0 +1,37 @@
+"use strict";
+
+class UserStorage {
+  // 서버 데이터
+  // static: 외부에서 클래그 안 변수를 바로 접근하고 싶을 때 사용(정적 변수로 만들어야함)
+  // 다른 곳에서 볼 수 없게 개인화 해야함(은닉화): #
+  static #users = {
+    id: ["HID1", "HID2", "HID3"],
+    psword: ["123", "1234", "12345"],
+    name: ["이름1", "이름2", "이름3"],
+  };
+
+  // 은닉화 된 변수를 메소드로 전달 함
+  static getUsers(...fields) {
+    const users = this.#users;
+    const newUsers = fields.reduce((newUsers, field) => {
+      if (users.hasOwnProperty(field)) {
+        newUsers[field] = users[field];
+      }
+      return newUsers;
+    }, {});
+    return newUsers;
+  };
+
+  static getUserInfo (id) {
+    const users = this.#users;
+    const idx = users.id.indexOf(id);
+    const usersKeys = Object.keys(users);
+    const userInfo = usersKeys.reduce((newUser, info) => {
+      newUser[info] = users[info][idx];
+      return newUser;
+    }, {});
+    return userInfo;
+  }
+};
+
+module.exports = UserStorage;
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,11 +1,6 @@
 "use strict";
 
-// 서버 데이터
-const users = {
-  id: ["HID1", "HID2", "HID3"],
-  psword: ["123", "1234", "12345"],
-};
-
+const User = require("../../models/User")
 
 // API로 만드는 중
 const output = {
@@ -19,35 +14,12 @@ const output = {
 
 const process = {
   login: (req, res) => {
-      const id = req.body.id;
-      const psword = req.body.psword;
-      // console.log(id, psword);
-
-      if (users.id.includes(id)) {
-        const idx = users.id.indexOf(id);
-        if (users.psword[idx] === psword) {
-          return res.json({
-            success: true,
-
-          })
-        }
-      }
-      // 프론트 엔드로 결과 보내줌
-      return res.json({
-        success: false,
-        msg: "로그인에 실패하셨습니다."
-      })
+    const user = new User(req.body);  // 인스턴스 형태
+    const response = user.login();
+    // console.log(response);
+    return res.json(response);
   },
 };
-
-// const home = (req, res) => {
-//   res.render("home/index");
-// };
-
-// const login = (req, res) => {
-//   res.render("home/login");
-// };
-
 
 module.exports = {
   output,
